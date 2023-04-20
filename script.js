4ed7eff871fd09cbe05068f581095744765b51e7
airianaj <abfe.am@gmail.com>
2023-04-21 04:56:58 +0900

    document.querySelectorAll
    JS7 スライド　19〜21
    HTMLから、引数で指定したセレクタに合致するものを全て取得

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function getElement(){
     element = document.querySelector('#shopB');
     console.log('選ばれたのは' + element.textContent + 'でした');
 }
+
+function getElement(){
+    elements = document.querySelectorAll('.shop li');
+    let len = elements.length;
+    for (let i = 0; i < len; i++){
+        console.log(elements.item(i).textContent);
+    }
+}
