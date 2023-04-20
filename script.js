6e2a5adfa9e8e8534d3c1d6f552e00858ed37820
airianaj <abfe.am@gmail.com>
2023-04-21 04:51:44 +0900

    document.querySelector
    JS7 スライド16〜18
    引数で指定したセレクタに合致するものを1つ取得する。

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let lists;
 document.getElementById("lists")
 console.log(document.getElementById("lists"));
+
+function getElement(){
+    element = document.querySelector('#shopB');
+    console.log('選ばれたのは' + element.textContent + 'でした');
+}
