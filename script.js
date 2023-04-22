3fa96810c23a137589eaf341a2ed3fd022397cdf
airianaj <abfe.am@gmail.com>
2023-04-22 23:55:00 +0900

    コンソールでイベントの確認
    getElementsByClassName

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-let lists;
-document.getElementById("lists")
-console.log(document.getElementById("lists"));
-
-function getElement(){
-    element = document.querySelector('#shopB');
-    console.log('選ばれたのは' + element.textContent + 'でした');
-}
-
-function getElement(){
-    elements = document.querySelectorAll('.shop li');
-    let len = elements.length;
-    for (let i = 0; i < len; i++){
-        console.log(elements.item(i).textContent);
-    }
-}
+let lists = document.getElementsByClassName("pull-down-list")
+console.log(lists);
