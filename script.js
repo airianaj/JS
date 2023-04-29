359fa3fd5d12b87bbf0621b0db21faabaf0fbca2
airianaj <abfe.am@gmail.com>
2023-04-29 14:34:40 +0900

    イベント発火基礎編
    JS基礎９の内容
    addEventListenerクリック、マウスホバー、マウスアウト

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists);
+const pullDownButton = document.getElementById("lists")
+
+pullDownButton.addEventListener('click',function(){
+    console.log("クリック");
+});
+
+pullDownButton.addEventListener('mouseover',function(){
+    console.log("乗る");
+});
+
+pullDownButton.addEventListener('mouseout',function(){
+    console.log("外れる");
+});
